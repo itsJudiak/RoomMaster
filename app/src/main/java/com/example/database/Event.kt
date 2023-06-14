0fe9e25ffa6d7291cf24e36745dad560ac7eab8b
itsJudiak <marekjudiak28@gmail.com>
2023-06-14 22:52:48 +0200

    landscape

--- app/src/main/java/com/example/database/Event.kt
+++ app/src/main/java/com/example/database/Event.kt
@@ -1,6 +1,5 @@
 package com.example.database
 
-import android.R
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
@@ -19,46 +18,37 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-/**
- * Fragment Event is used for creating events and storing them in the Firebase Realtime Database.
- */
 class Event : Fragment() {
     private lateinit var binding: FragmentEventBinding
     private lateinit var database: DatabaseReference
     private lateinit var spinner: Spinner
-    private lateinit var selectedDate: String
-    private lateinit var selectedTime: String
-    private var selectedDateText: TextView? = null
-    private var selectedTimeText: TextView? = null
-    private var storedTitle: String = ""
-    private var storedDescription: String = ""
-    private var storedSelectedDate: String = ""
-    private var storedSelectedTime: String = ""
+    private var selectedDate: String? = null
+    private var selectedTime: String? = null
 
-    /**
-     * Creates the view of the fragment.
-     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
         binding = FragmentEventBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // Get a reference to the Firebase Realtime Database
         database = FirebaseDatabase.getInstance().reference
 
+        // Set up the spinner for room selection
         setSpinner()
 
+        // Set up the click listeners for date and time selection buttons
         binding.pickDateBtn.setOnClickListener {
             showDatePickerDialog()
         }
-        binding.pickTimeBtn.setOnClickListener{
+        binding.pickTimeBtn.setOnClickListener {
             showTimePickerDialog()
         }
-        selectedDateText = binding.selectedDateTextView
-        selectedTimeText = binding.selectedTimeTextView
 
+        // Set up the click listener for event creation button
         binding.eventBtn.setOnClickListener {
             createEvent()
         }
@@ -66,124 +56,128 @@ class Event : Fragment() {
         return view
     }
 
-    /**
-     * Restores the values of the views when the view is created.
-     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Restore values if available
-        binding.editTextTitle.setText(storedTitle)
-        binding.editTextDescription.setText(storedDescription)
-        selectedDateText?.text = storedSelectedDate
-        selectedTimeText?.text = storedSelectedTime
+        selectedDate = savedInstanceState?.getString(KEY_SELECTED_DATE)
+        selectedTime = savedInstanceState?.getString(KEY_SELECTED_TIME)
+        updateSelectedDateTimeText()
     }
 
-    /**
-     * Stores the values of the views before the view is destroyed.
-     */
-    override fun onDestroyView() {
-        super.onDestroyView()
-
-        // Store values before view is destroyed
-        storedTitle = binding.editTextTitle.text.toString()
-        storedDescription = binding.editTextDescription.text.toString()
-        storedSelectedDate = selectedDateText?.text.toString()
-        storedSelectedTime = selectedTimeText?.text.toString()
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Save the selected date and time values
+        outState.putString(KEY_SELECTED_DATE, selectedDate)
+        outState.putString(KEY_SELECTED_TIME, selectedTime)
     }
 
-    /**
-     * Shows a time picker dialog to select the time and stores the selected time.
-     */
     private fun showTimePickerDialog() {
+        // Show a time picker dialog to select the time
         val timePickerDialog = TimePickerDialog(requireContext(), TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
             val formattedHourOfDay = hourOfDay.toString().padStart(2, '0')
             val formattedMinute = minute.toString().padStart(2, '0')
 
+            // Store the selected time
             selectedTime = "$formattedHourOfDay:$formattedMinute"
-            selectedTimeText?.text = "Selected Time: $selectedTime"
+            updateSelectedDateTimeText()
         }, 0, 0, false)
 
         timePickerDialog.show()
     }
 
-    /**
-     * Shows a date picker dialog to select the date and stores the selected date.
-     */
     private fun showDatePickerDialog() {
+        // Show a date picker dialog to select the date
         val datePickerDialog = DatePickerDialog(requireContext())
         datePickerDialog.setOnDateSetListener { view, year, month, dayOfMonth ->
             // The month returned by the date picker starts from 0 (January = 0), so increment it by 1
             val formattedMonth = (month + 1).toString().padStart(2, '0')
             val formattedDayOfMonth = dayOfMonth.toString().padStart(2, '0')
 
+            // Store the selected date
             selectedDate = "$year-$formattedMonth-$formattedDayOfMonth"
-            selectedDateText?.text = "Selected Date: $selectedDate"
+            updateSelectedDateTimeText()
         }
 
         datePickerDialog.show()
     }
 
-    /**
-     * Creates the event based on the input fields.
-     * Checks if all fields are filled, validates the selected date and time, and saves the event to the database.
-     */
     private fun createEvent() {
+        // Get the input values
         val title = binding.editTextTitle.text.toString().trim()
         val description = binding.editTextDescription.text.toString().trim()
         val room = spinner.selectedItem as String
 
+        // Validate the input fields
         if (title.isEmpty() || description.isEmpty() || selectedDate.isNullOrEmpty() || selectedTime.isNullOrEmpty()) {
             Toast.makeText(requireContext(), "Please fill in all the fields", Toast.LENGTH_SHORT).show()
             return
         }
 
+        // Parse the selected date and time
         val selectedDateTime = "$selectedDate $selectedTime"
-        val currentDate = getCurrentDateTime()
-
         val eventDateTime = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).parse(selectedDateTime)
-        val currentDateTime = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).parse(currentDate)
 
-        if (eventDateTime == null || currentDateTime == null || eventDateTime.before(currentDateTime)) {
+        // Check if the selected date and time is valid
+        if (eventDateTime == null || eventDateTime.before(Calendar.getInstance().time)) {
             Toast.makeText(requireContext(), "Please select a valid date and time", Toast.LENGTH_SHORT).show()
             return
         }
 
+        // Create the event object
         val event = DataEvent(title, description, room, selectedDate!!, selectedTime!!)
+
+        // Generate a unique key for the event in the database
         val eventKey = database.push().key
 
         if (eventKey != null) {
+            // Store the event in the database
             database.child(eventKey).setValue(event)
                 .addOnSuccessListener {
+                    // Clear the input fields and show a success message
                     binding.editTextTitle.text?.clear()
                     binding.editTextDescription.text?.clear()
-                    selectedDateText?.text = null
-                    selectedTimeText?.text = null
+                    selectedDate = null
+                    selectedTime = null
+                    updateSelectedDateTimeText()
                     Toast.makeText(requireContext(), "Event created successfully", Toast.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener {
+                    // Show an error message if event creation fails
                     Toast.makeText(requireContext(), "Failed to create event", Toast.LENGTH_SHORT).show()
                 }
         }
     }
 
-    /**
-     * Sets the values for the room selection spinner.
-     */
     private fun setSpinner() {
+        // Set up the spinner for room selection
         spinner = binding.spinnerRoom
         val roomOptions = listOf("Gerlach", "Rysy", "Kriváň")
-        val adapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, roomOptions)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, roomOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
     }
 
-    /**
-     * Returns the current date and time as a formatted string.
-     */
-    private fun getCurrentDateTime(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
-        val currentDateTime = Calendar.getInstance().time
-        return dateFormat.format(currentDateTime)
+    private fun updateSelectedDateTimeText() {
+        // Update the selected date and time in the text view
+        val selectedDateTime = StringBuilder()
+
+        if (!selectedDate.isNullOrEmpty()) {
+            selectedDateTime.append("Selected Date: $selectedDate")
+        }
+
+        if (!selectedTime.isNullOrEmpty()) {
+            if (selectedDateTime.isNotEmpty()) {
+                selectedDateTime.append("\n")
+            }
+            selectedDateTime.append("Selected Time: $selectedTime")
+        }
+
+        binding.selectedDateTimeTextView!!.text = selectedDateTime.toString()
+    }
+
+    companion object {
+        private const val KEY_SELECTED_DATE = "selected_date"
+        private const val KEY_SELECTED_TIME = "selected_time"
     }
 }
--- app/src/main/java/com/example/database/Login.kt
+++ app/src/main/java/com/example/database/Login.kt
@@ -2,7 +2,6 @@ package com.example.database
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -11,9 +10,9 @@ import com.example.database.databinding.ActivityLoginBinding
 class Login : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
 
-    public override fun onStart() {
+    override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
@@ -30,11 +29,11 @@ class Login : AppCompatActivity() {
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
 
-
         binding.btnLogin.setOnClickListener {
             loginUser()
         }
     }
+
     private fun loginUser() {
         val email = binding.editTextEmail.text.toString().trim()
         val password = binding.editTextPassword.text.toString().trim()
@@ -43,7 +42,8 @@ class Login : AppCompatActivity() {
             Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
             return
         }
-        //Voľne dostupná časť kodu z Firebase
+
+        // Authenticate user with Firebase
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -52,10 +52,8 @@ class Login : AppCompatActivity() {
                     startActivity(intent)
                     finish()
                 } else {
-                    Toast.makeText(this, "Wrong data", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Wrong email or password", Toast.LENGTH_SHORT).show()
                 }
             }
     }
-
 }
-
